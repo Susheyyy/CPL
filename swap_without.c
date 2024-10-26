9708f5c3df4533f68c1037a6cd44a24a25313fb5
Sushmita Das <65953343+Susheyyy@users.noreply.github.com>
2024-10-26 23:59:29 +0530

    experiment 1

---
+++ swap_without.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main ()
+{
+    int a, b;
+    
+    printf ("Enter values for a and b");
+    scanf("%d%d", &a, &b);
+
+    a = a+b;
+    b = a-b;
+    a = a-b;
+
+    Printf ("Numbers after swapping %d%d", a,b);
+
+}
