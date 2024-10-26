11e58386f2ed2d375ed744799bf1973fec73f770
Sushmita Das <65953343+Susheyyy@users.noreply.github.com>
2024-10-26 23:56:26 +0530

    experiment 2

---
+++ odd_even-conditional-operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    int num;
+    
+    printf("Enter any number.");
+    scanf("%d", &num);
+
+ (num%2==0)?printf("The number is even"):printf("The number is odd");
+  
+  }
