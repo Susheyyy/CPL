d95ae5d5d5651a52c3a805713ccbe3893c6e78d7
Sushmita Das <65953343+Susheyyy@users.noreply.github.com>
2024-11-10 14:55:11 +0530

    experiment 2

--- odd_even-modulus.c
+++ odd_even-modulus.c
@@ -5,16 +5,16 @@ void main ()
 {
     int num;
     
-    printf("Enter any number.");
+    printf("Enter any number. \n");
     scanf("%d", &num);
 
   if(num%2==0)
   {
-    printf("Number %d is even", num);
+    printf("%d is even", num);
   }
   else
   {
-     printf("Number %d is odd", num);
+     printf("%d is odd", num);
   }
   }
   
